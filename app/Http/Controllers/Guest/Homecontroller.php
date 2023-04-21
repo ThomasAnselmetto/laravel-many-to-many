3a5e5dfec06e3144f23a798fb0b341febc34c3ab
ThomasAnselmetto <t.anselmetto@gmail.com>
2023-04-21 16:02:59 +0200

    home changes not working

--- app/Http/Controllers/Guest/Homecontroller.php
+++ app/Http/Controllers/Guest/Homecontroller.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class Homecontroller extends Controller
 {
    public function index() {
-        return view('guest.home');
+    //    $recent_projects = Project::where('published',1)->orderBy('updated_at','DESC')->limit(8)->get();
+    //    $highlight_projects = Project::where('published',1)->orderBy('updated_at','DESC')->limit(8)->get();
+    //     return view('guest.home','recent_projects');
     }
 }
